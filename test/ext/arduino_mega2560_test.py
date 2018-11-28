d81f7d0b23b9989aedc358a2b0b22825354bbbd0
MAKEBLOCK\mk10244 <zhangweiyu@makeblock.com>
2018-11-28 16:57:49 +0800

    arduino_mega2560：断言添加说明

--- test/ext/arduino_mega2560_test.py
+++ test/ext/arduino_mega2560_test.py
@@ -21,175 +21,181 @@ class ArduinoMega2560Test(unittest.TestCase):
         f = open(cls.path, 'r')
         test_file = data_handle.byteify(json.load(f))
         cls.test_dict = test_file['ext-i18n/arduino_mega2560']
+    
+    def check_key_exists(self, key):
+        self.assertIn(key, self.test_dict, '\n缺少key: {0}'.format(key))
+
+    def check_expect_value(self, key, expect_value):
+        test_data = self.test_dict[key]
+        self.assertEqual(test_data, expect_value, '\nkey: {0}, value:{1}, error: 值不等于{2}, '.format(key, test_data, expect_value))
+
+    def check_param(self, key, param):
+        test_data = self.test_dict[key]
+        self.assertIn(param, test_data, '\nkey:{0}, 缺少参数：{1}'.format(key, param))
+
+    def check_icon(self, key):
+        test_data = self.test_dict[key]
+        self.assertIn('[ICON]', test_data, '\nkey: {0}, 缺少参数：[ICON]'.format(key))
+        self.assertEqual(test_data.index('[ICON]'), 0, '\nkey: {0}, error:参数[ICON]必须在首位'.format(key))
 
     # ext-i18n/arduino_mega2560/No empty value
-    def test_ext_i18n_arduino_mega2560_no_empty_value(self):
+    def test_no_empty_value(self):
         for key,value in self.test_dict.items():
-            self.assertIsNotNone(value)
-            self.assertNotEqual(value, '')
+            self.assertIsNotNone(value, "\nkey: {0}, error: 值不能为空".format(key))
+            self.assertNotEqual(value, "", "\nkey: {0}, error: 值不能为空".format(key))
 
     # ext-i18n/arduino_mega2560/No new or missing items
-    def test_ext_i18n_arduino_mega2560_no_new_or_missing_items(self):
-        self.assertEqual(len(self.test_dict), 68)
+    def test_no_new_or_missing_items(self):
+        self.assertEqual(len(self.test_dict), 68, 'key总数不对，请检查！')
 
     # ext-i18n/arduino_mega2560/extensionName equals "Arduino Mega2560"
-    def test_ext_i18n_arduino_mega2560_extensionName_equals_Arduino_Mega2560(self):
-        self.assertIn('extensionName', self.test_dict)
-        test_data = self.test_dict['extensionName']
-        self.assertEqual(test_data, "Arduino Mega2560")
+    def test_extensionName(self):
+        key = 'extensionName'
+        expect_value = "Arduino Mega2560"
+        self.check_key_exists(key)
+        self.check_expect_value(key, expect_value)
+        
 
     # ext-i18n/arduino_mega2560/getDigital contains [ICON] [PORT]
-    def test_ext_i18n_arduino_mega2560_getDigital(self):
-        self.assertIn('getDigital', self.test_dict)
-        test_data = self.test_dict['getDigital']
-        self.assertIn('[PORT]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_getDigital(self):
+        key = 'getDigital'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[PORT]')
 
     # ext-i18n/arduino_mega2560/getAnalog contains [ICON] [PORT]
-    def test_ext_i18n_arduino_mega2560_getAnalog(self):
-        self.assertIn('getAnalog', self.test_dict)
-        test_data = self.test_dict['getAnalog']
-        self.assertIn('[PORT]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_getAnalog(self):
+        key = 'getAnalog'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[PORT]')
 
     # ext-i18n/arduino_mega2560/getPulse contains [ICON] [PORT] [TIME]
-    def test_ext_i18n_arduino_mega2560_getPulse(self):
-        self.assertIn('getPulse', self.test_dict)
-        test_data = self.test_dict['getPulse']
-        self.assertIn('[PORT]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertIn('[TIME]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_getPulse(self):
+        key = 'getPulse'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[PORT]')
+        self.check_param(key, '[TIME]')
 
     # ext-i18n/arduino_mega2560/setDigitale contains [ICON] [PORT] [LEVEL]
-    def test_ext_i18n_arduino_mega2560_setDigital(self):
-        self.assertIn('setDigital', self.test_dict)
-        test_data = self.test_dict['setDigital']
-        self.assertIn('[PORT]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertIn('[LEVEL]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_setDigital(self):
+        key = 'setDigital'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[PORT]')
+        self.check_param(key, '[LEVEL]')
 
     # ext-i18n/arduino_mega2560/setPwm contains [ICON] [PORT] [POWER]
-    def test_ext_i18n_arduino_mega2560_setPwm(self):
-        self.assertIn('setPwm', self.test_dict)
-        test_data = self.test_dict['setPwm']
-        self.assertIn('[PORT]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertIn('[POWER]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_setPwm(self):
+        key = 'setPwm'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[PORT]')
+        self.check_param(key, '[POWER]')
 
     # ext-i18n/arduino_mega2560/setTone contains [ICON] [PORT] [NOTE] [BEAT]
-    def test_ext_i18n_arduino_mega2560_setTone(self):
-        self.assertIn('setTone', self.test_dict)
-        test_data = self.test_dict['setTone']
-        self.assertIn('[PORT]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertIn('[BEAT]', test_data)
-        self.assertIn('[NOTE]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_setTone(self):
+        key = 'setTone'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[PORT]')
+        self.check_param(key, '[BEAT]')
+        self.check_param(key, '[NOTE]')
 
     # ext-i18n/arduino_mega2560/setServo contains [ICON] [PORT] [ANGLE]
-    def test_ext_i18n_arduino_mega2560_setServo(self):
-        self.assertIn('setServo', self.test_dict)
-        test_data = self.test_dict['setServo']
-        self.assertIn('[PORT]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertIn('[ANGLE]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_setServo(self):
+        key = 'setServo'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[PORT]')
+        self.check_param(key, '[ANGLE]')
+
 
     # ext-i18n/arduino_mega2560/serialWrite contains [ICON] [TEXT]
-    def test_ext_i18n_arduino_mega2560_serialWrite(self):
-        self.assertIn('serialWrite', self.test_dict)
-        test_data = self.test_dict['serialWrite']
-        self.assertIn('[TEXT]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_serialWrite(self):
+        key = 'serialWrite'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[TEXT]')
 
     # ext-i18n/arduino_mega2560/getSerialAvailable contains [ICON] 
-    def test_ext_i18n_arduino_mega2560_getSerialAvailable(self):
-        self.assertIn('getSerialAvailable', self.test_dict)
-        test_data = self.test_dict['getSerialAvailable']
-        self.assertIn('[ICON]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_getSerialAvailable(self):
+        key = 'getSerialAvailable'
+        self.check_key_exists(key)
+        self.check_icon(key)
 
     # ext-i18n/arduino_mega2560/serialRead contains [ICON] 
-    def test_ext_i18n_arduino_mega2560_serialRead(self):
-        self.assertIn('serialRead', self.test_dict)
-        test_data = self.test_dict['serialRead']
-        self.assertIn('[ICON]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_serialRead(self):
+        key = 'serialRead'
+        self.check_key_exists(key)
+        self.check_icon(key)
 
     # ext-i18n/arduino_mega2560/getUltrasonic contains [ICON] [PORT1] [PORT2]
-    def test_ext_i18n_arduino_mega2560_getUltrasonic(self):
-        self.assertIn('getUltrasonic', self.test_dict)
-        test_data = self.test_dict['getUltrasonic']
-        self.assertIn('[PORT1]', test_data)
-        self.assertIn('[ICON]', test_data)
-        self.assertIn('[PORT2]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_getUltrasonic(self):
+        key = 'getUltrasonic'
+        self.check_key_exists(key)
+        self.check_icon(key)
+        self.check_param(key, '[PORT1]')
+        self.check_param(key, '[PORT2]')
 
     # ext-i18n/arduino_mega2560/getTimer contains [ICON] 
-    def test_ext_i18n_arduino_mega2560_getTimer(self):
-        self.assertIn('getTimer', self.test_dict)
-        test_data = self.test_dict['getTimer']
-        self.assertIn('[ICON]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_getTimer(self):
+        key = 'getTimer'
+        self.check_key_exists(key)
+        self.check_icon(key)
 
     # ext-i18n/arduino_mega2560/resetTimer contains [ICON] 
-    def test_ext_i18n_arduino_mega2560_resetTimer(self):
-        self.assertIn('resetTimer', self.test_dict)
-        test_data = self.test_dict['resetTimer']
-        self.assertIn('[ICON]', test_data)
-        self.assertEqual(test_data.index('[ICON]'), 0)
+    def test_mega2560_resetTimer(self):
+        key = 'resetTimer'
+        self.check_key_exists(key)
+        self.check_icon(key)
 
     # ext-i18n/arduino_mega2560/SETTONE_NOTE is right
-    def test_ext_i18n_arduino_mega2560_SETTONE_NOTE(self):
-        self.assertEqual(self.test_dict['SETTONE_NOTE_0'], "C2")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_1'], "D2")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_2'], "E2")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_3'], "F2")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_4'], "G2")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_5'], "A2")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_6'], "B2")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_7'], "C3")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_8'], "D3")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_9'], "E3")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_10'], "F3")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_11'], "G3")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_12'], "A3")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_13'], "B3")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_14'], "C4")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_15'], "D4")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_16'], "E4")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_17'], "F4")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_18'], "G4")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_19'], "A4")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_20'], "B4")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_21'], "C5")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_22'], "D5")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_23'], "E5")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_24'], "F5")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_25'], "G5")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_26'], "A5")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_27'], "B5")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_28'], "C6")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_29'], "D6")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_30'], "E6")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_31'], "F6")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_32'], "G6")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_33'], "A6")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_34'], "B6")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_35'], "C7")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_36'], "D7")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_37'], "E7")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_38'], "F7")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_39'], "G7")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_40'], "A7")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_41'], "B7")
-        self.assertEqual(self.test_dict['SETTONE_NOTE_42'], "C8")
+    def test_SETTONE_NOTE(self):
+        self.check_expect_value('SETTONE_NOTE_0', "C2")
+        self.check_expect_value('SETTONE_NOTE_1', "D2")
+        self.check_expect_value('SETTONE_NOTE_2', "E2")
+        self.check_expect_value('SETTONE_NOTE_3', "F2")
+        self.check_expect_value('SETTONE_NOTE_4', "G2")
+        self.check_expect_value('SETTONE_NOTE_5', "A2")
+        self.check_expect_value('SETTONE_NOTE_6', "B2")
+        self.check_expect_value('SETTONE_NOTE_7', "C3")
+        self.check_expect_value('SETTONE_NOTE_8', "D3")
+        self.check_expect_value('SETTONE_NOTE_9', "E3")
+        self.check_expect_value('SETTONE_NOTE_10', "F3")
+        self.check_expect_value('SETTONE_NOTE_11', "G3")
+        self.check_expect_value('SETTONE_NOTE_12', "A3")
+        self.check_expect_value('SETTONE_NOTE_13', "B3")
+        self.check_expect_value('SETTONE_NOTE_14', "C4")
+        self.check_expect_value('SETTONE_NOTE_15', "D4")
+        self.check_expect_value('SETTONE_NOTE_16', "E4")
+        self.check_expect_value('SETTONE_NOTE_17', "F4")
+        self.check_expect_value('SETTONE_NOTE_18', "G4")
+        self.check_expect_value('SETTONE_NOTE_19', "A4")
+        self.check_expect_value('SETTONE_NOTE_20', "B4")
+        self.check_expect_value('SETTONE_NOTE_21', "C5")
+        self.check_expect_value('SETTONE_NOTE_22', "D5")
+        self.check_expect_value('SETTONE_NOTE_23', "E5")
+        self.check_expect_value('SETTONE_NOTE_24', "F5")
+        self.check_expect_value('SETTONE_NOTE_25', "G5")
+        self.check_expect_value('SETTONE_NOTE_26', "A5")
+        self.check_expect_value('SETTONE_NOTE_27', "B5")
+        self.check_expect_value('SETTONE_NOTE_28', "C6")
+        self.check_expect_value('SETTONE_NOTE_29', "D6")
+        self.check_expect_value('SETTONE_NOTE_30', "E6")
+        self.check_expect_value('SETTONE_NOTE_31', "F6")
+        self.check_expect_value('SETTONE_NOTE_32', "G6")
+        self.check_expect_value('SETTONE_NOTE_33', "A6")
+        self.check_expect_value('SETTONE_NOTE_34', "B6")
+        self.check_expect_value('SETTONE_NOTE_35', "C7")
+        self.check_expect_value('SETTONE_NOTE_36', "D7")
+        self.check_expect_value('SETTONE_NOTE_37', "E7")
+        self.check_expect_value('SETTONE_NOTE_38', "F7")
+        self.check_expect_value('SETTONE_NOTE_39', "G7")
+        self.check_expect_value('SETTONE_NOTE_40', "A7")
+        self.check_expect_value('SETTONE_NOTE_41', "B7")
+        self.check_expect_value('SETTONE_NOTE_42', "C8")
  
  
 if __name__ == "__main__":
